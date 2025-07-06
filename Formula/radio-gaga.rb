43fdb5f1ac17573c453e8993a7be0d40ac09ceff
githubpoet <github.poet204@passmail.net>
2025-07-06 14:01:28 +0200

    Update to v1.1.1 with proper requirements.txt dependency handling

--- Formula/radio-gaga.rb
+++ Formula/radio-gaga.rb
@@ -3,42 +3,12 @@ class RadioGaga < Formula
 
   desc "Curses-based TUI radio streaming application"
   homepage "https://github.com/githubpoet/radio-gaga"
-  url "https://github.com/githubpoet/radio-gaga/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "185f6a9eca2bd7c229f013ceba9b7ee544847be4548ae2980ea84455350347cf"
+  url "https://github.com/githubpoet/radio-gaga/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "895488ddd29c5efe55bf495637eb3bcc94c4cbce343d1fc9839da68a5aa47010"
   license "MIT"
 
   depends_on "ffmpeg"
   depends_on "python@3.11"
-
-  resource "requests" do
-    url "https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3/requests-2.31.0.tar.gz"
-    sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
-  end
-
-  resource "pyyaml" do
-    url "https://files.pythonhosted.org/packages/cd/e5/af35f7ea75cf72f2cd079c95ee16797de7cd71f29ea7c68ae5ce7be1eda0/PyYAML-6.0.1.tar.gz"
-    sha256 "bfdf460b1736c775f2ba9f6a92bca30bc2095067b8a9d77876d1fad6cc3b4a43"
-  end
-
-  resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/ed/63/22ba4ebfe7430b76388e7cd448d5478814d3032121827c12a2cc287e2260/urllib3-2.2.3.tar.gz"
-    sha256 "e7d814a81dad81e6caf2ec9fdedb284ecc9c73076b62654547cc64ccdcae26e9"
-  end
-
-  resource "certifi" do
-    url "https://files.pythonhosted.org/packages/c2/02/a95f2b11e207f68bc64d7aae9666fed2e2b3f307748d5123dffb72a1bbea/certifi-2024.7.4.tar.gz"
-    sha256 "5a1e7645bc0ec61a09e26c36f6106c19912aee950e6a9b7b7b35b2a6b3f5b2f6"
-  end
-
-  resource "charset-normalizer" do
-    url "https://files.pythonhosted.org/packages/63/09/c1bc53dab74b1816a00d8d030de5bf98f724c52c1635e07681d312f20be8/charset-normalizer-3.3.2.tar.gz"
-    sha256 "f30c3cb33b24454a82faecaf01b19c18562b1e89558fb6c56de4d9118a032fd5"
-  end
-
-  resource "idna" do
-    url "https://files.pythonhosted.org/packages/21/ed/f86a79a07470cb07819390452f178b3bef1d375f2ec021ecfc709fc7cf07/idna-3.7.tar.gz"
-    sha256 "028ff3aadf0609c1fd278d8ea3089299412a7a8b9bd005dd08b9f8285bcb5cfc"
-  end
 
   def install
     virtualenv_install_with_resources
